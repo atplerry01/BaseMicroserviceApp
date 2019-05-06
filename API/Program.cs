7e5070456b108c5e21d76633148bc1f107b47442
chrisgya <christiangyaban@hotmail.com>
2019-05-06 22:35:07 +0100

    first commit

---
+++ API/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Enrichers.AspnetcoreHttpcontext;
+using LoggerClassLib;
+
+namespace API
+{
+    public class Program
+    {
+        public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+          .SetBasePath(Directory.GetCurrentDirectory())
+          .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+          .AddEnvironmentVariables()
+          .Build();
+
+
+        public static int Main(string[] args)
+        {
+
+            try
+            {
+                CreateWebHostBuilder(args).Build().Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Host terminated unexpectedly");
+                Console.Write(ex.ToString());
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseSerilog((provider, context, loggerConfig) =>
+                {
+                    loggerConfig.WithSerilogConfiguration(provider, "BaseMicroserviceAPI", Configuration);
+                });
+        }
+
+    }
+}
---
+++ LoggerClassLib/UserInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoggerClassLib
+{
+    public class UserInfo
+    {
+        public string UserName { get; set; }
+        public string UserId { get; set; }
+        public Dictionary<string, List<string>> UserClaims { get; set; }
+    }
+}
